06d259a4dd162989da99268bd66ae542bcbf92e6
Margot Rasamy <rasamymargot@gmail.com>
2022-04-10 21:38:35 +0200

    Added card animation

--- frontend/src/components/CryptoCard.js
+++ frontend/src/components/CryptoCard.js
@@ -1,16 +1,26 @@
 import Ethereum from '../assets/images/ethereum.png'
-import { useContext } from 'react';
+import { useContext, useEffect, useRef } from 'react';
 import { TransactionContext } from '../context/TransactionContext';
+import { VanillaTilt } from '../utils/CardTilt';
 
 const CryptoCard = () => {
-
     const { connectedAccounts } = useContext(TransactionContext);
 
     const isConnected = () => {
         return !!connectedAccounts.length
     }
+
+    useEffect(() => {
+        VanillaTilt.init(document.querySelector(".crypto-card"), {
+            max: 25,
+            speed: 350,
+            glare: true,
+            "max-glare": 0.2
+        });
+    }, []);
+
     return (
-        <div className="crypto-card">
+        <div className="crypto-card" data-tilt>
             
             <div className="image-wrapper">
                 <img src={Ethereum} />
